9f338bbfebce32456c5799cb3204f3fc8899f29a
vikas kaundal <vikaskaundal21@gmail.com>
2024-04-30 00:35:24 +0530

    create the offcanvas slider

--- js/global.js
+++ js/global.js
@@ -4,17 +4,58 @@ const hamburger = document.querySelector(".hamburger");
 const overlay = document.querySelector(".overlay");
 
 hamburger.addEventListener("click", () => {
-  mobile_offcanvas.classList.add("open");
-  overlay.classList.add("open_overlay");
+    mobile_offcanvas.classList.add("open");
+    overlay_add();
 });
 
 close_btn.addEventListener("click", () => {
-  mobile_offcanvas.classList.remove("open");
-  overlay.classList.remove("open_overlay");
-});
-overlay.addEventListener("click",()=>{
     mobile_offcanvas.classList.remove("open");
+    overlay_remove();
+});
+
+function overlay_add(){
+    overlay.classList.add("open_overlay");
+    overlay.addEventListener("click", overlay_remove);
+}
+
+function overlay_remove(){
     overlay.classList.remove("open_overlay");
+    overlay.removeEventListener("click", overlay_remove);
+}
+
+overlay.addEventListener("click", () => {
+    mobile_offcanvas.classList.remove("open");
+    cart_slider_off_canvas.classList.remove("open_cart");
+    overlay_remove();
+});
+
+
+// cart slider js
+const cart = document.querySelector(".cart");
+const cart_slider_off_canvas = document.querySelector(".cart_slider_off_canvas");
+const close_btn_cart = document.querySelector(".close_btn_cart");
+
+cart.addEventListener("click", () => {
+    cart_slider_off_canvas.classList.add("open_cart");
+    overlay_add()
+});
+close_btn_cart.addEventListener("click", () => {
+    cart_slider_off_canvas.classList.remove("open_cart");
+    overlay_remove()
+});
+
+
+
+// -----------------This is for accordion toggeling-----------------------
+document.addEventListener("DOMContentLoaded", function () {
+    var accordionHeaders = document.querySelectorAll(".accordion-header");
+
+    accordionHeaders.forEach(function (header) {
+        header.addEventListener("click", function () {
+            var accordionItem = this.parentElement;
+            var accordionContent = accordionItem.querySelector(".accordion-content");
+            accordionContent.style.display =
+                accordionContent.style.display === "block" ? "none" : "block";
+        });
     });
-
-
+});
